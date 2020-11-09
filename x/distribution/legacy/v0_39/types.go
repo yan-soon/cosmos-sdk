dfe58d27d346638b9fd7b89269e785ffed9aaf06
Ivan Poon <ravenxce@gmail.com>
2020-11-09 15:47:14 +0800

    Add legacy params

--- x/distribution/legacy/v0_39/types.go
+++ x/distribution/legacy/v0_39/types.go
@@ -5,3 +5,10 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 type FeePool struct {
 	CommunityPool sdk.DecCoins `json:"community_pool" yaml:"community_pool"` // pool for community funds yet to be spent
 }
+
+type Params struct {
+	CommunityTax        sdk.Dec `json:"community_tax" yaml:"community_tax"`
+	BaseProposerReward  sdk.Dec `json:"base_proposer_reward" yaml:"base_proposer_reward"`
+	BonusProposerReward sdk.Dec `json:"bonus_proposer_reward" yaml:"bonus_proposer_reward"`
+	WithdrawAddrEnabled bool    `json:"withdraw_addr_enabled" yaml:"withdraw_addr_enabled"`
+}
