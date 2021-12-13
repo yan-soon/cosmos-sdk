0c07019c1a96df381e6a27a581c0be1db96deae6
holyxiaoxin <jr.loves.maths@gmail.com>
2021-12-13 15:32:06 +0800

    feat: disable bank/multi-send

--- x/bank/handler.go
+++ x/bank/handler.go
@@ -19,9 +19,10 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 			res, err := msgServer.Send(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
 
-		case *types.MsgMultiSend:
-			res, err := msgServer.MultiSend(sdk.WrapSDKContext(ctx), msg)
-			return sdk.WrapServiceResult(ctx, res, err)
+		// disable multi-send as we ignore events from InputOutputCoins
+		// case *types.MsgMultiSend:
+		// 	res, err := msgServer.MultiSend(sdk.WrapSDKContext(ctx), msg)
+		// 	return sdk.WrapServiceResult(ctx, res, err)
 
 		default:
 			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized bank message type: %T", msg)
