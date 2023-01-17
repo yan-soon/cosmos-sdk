c7443036ea82eb271b30750948814205128543bd
Randy <randy75828@gmail.com>
2023-01-17 11:17:22 +0800

    move extraction of address from key to be in app logic rather than in accountKeeper

--- x/auth/keeper/account.go
+++ x/auth/keeper/account.go
@@ -172,23 +172,19 @@ func (ak AccountKeeper) IterateEthToCosmosAddressMapping(ctx sdk.Context, cb fun
 
 	defer iterator.Close()
 	for ; iterator.Valid(); iterator.Next() {
-		addressKey := make([]byte, len(iterator.Key())-len(types.KeyPrefix(types.EthAddressToCosmosAddressKey)))
-		copy(addressKey, iterator.Key()[len(types.KeyPrefix(types.EthAddressToCosmosAddressKey)):])
-		if cb(addressKey, iterator.Value()) {
+		if cb(iterator.Key(), iterator.Value()) {
 			break
 		}
 	}
 
 }
-func (ak AccountKeeper) IterateCosmosToEthAddressMapping(ctx sdk.Context, cb func(cosmosAddress, ethAddress sdk.AccAddress) bool) {
+func (ak AccountKeeper) IterateCosmosToEthAddressMapping(ctx sdk.Context, cb func(key []byte, ethAddress sdk.AccAddress) bool) {
 	store := ctx.KVStore(ak.key)
 	iterator := sdk.KVStorePrefixIterator(store, types.KeyPrefix(types.CosmosAddressToEthAddressKey))
 
 	defer iterator.Close()
 	for ; iterator.Valid(); iterator.Next() {
-		addressKey := make([]byte, len(iterator.Key())-len(types.KeyPrefix(types.CosmosAddressToEthAddressKey)))
-		copy(addressKey, iterator.Key()[len(types.KeyPrefix(types.CosmosAddressToEthAddressKey)):])
-		if cb(addressKey, iterator.Value()) {
+		if cb(iterator.Key(), iterator.Value()) {
 			break
 		}
 	}
